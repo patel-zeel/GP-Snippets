8842e4cb60863b0ab9d192db2c81e94f40049017
Zeel B Patel <patel_zeel@iitgn.ac.in>
2021-09-03 21:07:20 +0530

    Update kernels.py

--- pytorch/kernels.py
+++ pytorch/kernels.py
@@ -3,7 +3,7 @@ import torch
 ### RBF
 
 def RBF_func(X1, X2, l):
-  scaled_dist = (X1[:,None,:] - X2[None,:,:])/l.reshape(1,1,-1)
+    scaled_dist = (X1[:,None,:] - X2[None,:,:])/l.reshape(1,1,-1)
   
-  return torch.exp(-0.5 * torch.square(scaled_dist))
+    return torch.exp(-0.5 * torch.square(scaled_dist))
   
