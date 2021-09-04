47d98d80d161c363cb0d280c53bc2f0c10d373b6
Zeel B Patel <patel_zeel@iitgn.ac.in>
2021-09-04 06:04:47 +0530

    small edit

--- pytorch/kernels.py
+++ pytorch/kernels.py
@@ -2,10 +2,10 @@ import torch
 
 ### RBF
 
-def RBF_func(X1, X2, l):
-    X1 = X1/l.reshape(1,-1)
-    X2 = X2/l.reshape(1,-1)
-    scaled_dist = (X1[:,None,:] - X2[None,:,:])
+def RBF_func(Xi, Xj, l):
+    Xi = Xi/l.reshape(1,-1)
+    Xj = Xj/l.reshape(1,-1)
+    scaled_dist = (Xi[:,None,:] - Xj[None,:,:])
   
     return torch.exp(-0.5 * torch.square(scaled_dist))
   
